1e3411e7e708243490ea394856af51cb1ddbbbe0
Радмир Биктимиров <tembr.com@gmail.com>
2023-08-20 21:10:24 +0300

    create new project

---
+++ ST.EplAddins.SymbolVariants/ChangeSymbolAction.cs
@@ -0,0 +1,30 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST.EplAddins.SymbolVariants
+{
+    class ChangeSymbolAction : IEplAction
+    {
+        public static string actionName = "ChangeVariantsSymbol";
+        public bool Execute(ActionCallingContext oActionCallingContext)
+        {
+            return true;
+        }
+
+        public void GetActionProperties(ref ActionProperties actionProperties)
+        {
+            
+        }
+
+        public bool OnRegister(ref string Name, ref int Ordinal)
+        {
+            Name = actionName;
+            Ordinal = 32;
+            return true;
+        }
+    }
+}
---
+++ ST.EplAddins.SymbolVariants/SymbolAddinModule.cs
@@ -0,0 +1,41 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.Gui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST.EplAddins.SymbolVariants
+{
+    public class SymbolAddinModule : IEplAddIn
+    {
+        public bool OnExit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OnInit()
+        {
+            return true;
+        }
+
+        public bool OnInitGui()
+        {
+            Menu menu = new Menu();
+            menu.AddMainMenu("Scan ", Menu.MainMenuName.eMainMenuUtilities, "Change symbol", actionName, "Статус", 1);
+        }
+
+        public bool OnRegister(ref bool bLoadOnStart)
+        {
+            bLoadOnStart = true;
+            System.Windows.Forms.MessageBox.Show("Symbol addin is implemented");
+            return true;
+        }
+
+        public bool OnUnregister()
+        {
+            return true;
+        }
+    }
+}
