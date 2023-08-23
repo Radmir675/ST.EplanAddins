87d41cc40ae4778e782401b211db422d598be8c4
Радмир Биктимиров <tembr.com@gmail.com>
2023-08-23 16:47:35 +0300

    Исправлены ошибки по Build x64

--- ST.EplAddins.SymbolVariants/ChangeSymbolAction.cs
+++ ST.EplAddins.SymbolVariants/ChangeSymbolAction.cs
@@ -4,10 +4,7 @@ using Eplan.EplApi.DataModel;
 using Eplan.EplApi.DataModel.MasterData;
 using Eplan.EplApi.HEServices;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ST.EplAddins.SymbolVariants
@@ -20,60 +17,65 @@ namespace ST.EplAddins.SymbolVariants
             Name = actionName;
             Ordinal = 32;
             return true;
-            Settings oSettings = new Settings();
-
-
-
         }
         public bool Execute(ActionCallingContext oActionCallingContext)
         {
-            SelectionSet selectionSet = new SelectionSet();
-            Project currentProject = selectionSet.GetCurrentProject(true);
-            var userSelection = selectionSet.Selection;
-
-            if (userSelection.Count() != 1)
-            {
-                MessageBox.Show("Пожалуйста выберите лишь 1 объект");
-                return false;
-            }
-
-            StorableObject storableObject = userSelection.Single();
-            SymbolLibrary symbolLibrary = new SymbolLibrary();
-            SymbolReference symbolref = storableObject as SymbolReference;
-
-            Symbol parent = symbolref.SymbolVariant.Parent;
-
-            int intSymbolCurrent = symbolref.SymbolVariant.VariantNr;//отсчет идет от 0
-            int symbolsCount = parent.Variants.Count();//общее число
-            int intSymbolNext = intSymbolCurrent < symbolsCount - 1 ? intSymbolNext = intSymbolCurrent + 1 : 0;
-
-            var currentVariant = symbolref.SymbolVariant;
-
-            string symbolLibraryName = symbolref.SymbolVariant.SymbolLibraryName;
-            string symbolName = symbolref.SymbolVariant.SymbolName;
-
-
-            var currentSymbol = currentProject.SymbolLibraries
-                .Where(x => x.Name == symbolLibraryName)
-                .Select(x => x.Symbols.Where(symbolNamez => symbolNamez.Name == symbolName)).Single();
-
-            SymbolVariant symbolVariantToReplace = null;
-            try
-            {
-                symbolVariantToReplace = currentSymbol.Select(c => c.Variants.Single(g => g.VariantNr == intSymbolNext)).Single();
-
-            }
-            catch (Exception)
+            using (new LockingStep())
             {
 
-                symbolVariantToReplace = currentSymbol.Select(c => c.Variants.Single(g => g.VariantNr == 0)).Single();
+                SelectionSet selectionSet = new SelectionSet();
+                Project currentProject = selectionSet.GetCurrentProject(true);
+                var userSelection = selectionSet.Selection;
+                // TODO отсортировать объекты одного типа(символы)
+                //TODO если символы одного имени то для каждого применить операцию вращения
+
+                if (userSelection.Count() != 1)
+                {
+                    MessageBox.Show("Пожалуйста выберите лишь 1 объект");
+                    return false;
+                }
+
+                using (SafetyPoint safetyPoint = SafetyPoint.Create())
+                {
+
+                    StorableObject storableObject = userSelection.Single();
+                    SymbolLibrary symbolLibrary = new SymbolLibrary();
+                    SymbolReference symbolref = storableObject as SymbolReference;
+
+                    Symbol parent = symbolref.SymbolVariant.Parent;
+
+                    int intSymbolCurrent = symbolref.SymbolVariant.VariantNr;//отсчет идет от 0
+                    int symbolsCount = parent.Variants.Count();//общее число
+                    int intSymbolNext = intSymbolCurrent < symbolsCount - 1 ? intSymbolNext = intSymbolCurrent + 1 : 0;
+
+                    var currentVariant = symbolref.SymbolVariant;
+
+                    string symbolLibraryName = symbolref.SymbolVariant.SymbolLibraryName;
+                    string symbolName = symbolref.SymbolVariant.SymbolName;
+
+
+                    var currentSymbol = currentProject.SymbolLibraries
+                        .Where(x => x.Name == symbolLibraryName)
+                        .Select(x => x.Symbols.Where(symbolNamez => symbolNamez.Name == symbolName)).Single();
+
+                    SymbolVariant symbolVariantToReplace = null;
+                    try
+                    {
+                        symbolVariantToReplace = currentSymbol.Select(c => c.Variants.Single(g => g.VariantNr == intSymbolNext)).Single();
+
+                    }
+                    catch (Exception)
+                    {
+                        symbolVariantToReplace = currentSymbol.Select(c => c.Variants.Single(g => g.VariantNr == 0)).Single();
+                    }
+                    symbolref.SymbolVariant = symbolVariantToReplace;
+                    safetyPoint.Commit();
+                }
 
             }
-
-            symbolref.SymbolVariant = symbolVariantToReplace;
-
             return true;
         }
+        //TODO: допилить множественное изменение если это один и тот же символ
 
         public void GetActionProperties(ref ActionProperties actionProperties)
         {
