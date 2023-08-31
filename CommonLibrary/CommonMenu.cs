a36db329286550113212f0d6bf06a1e9b723c196
Радмир Биктимиров <tembr.com@gmail.com>
2023-08-31 18:09:59 +0300

    Создание нового проекта

---
+++ CommonLibrary/CommonMenu.cs
@@ -0,0 +1,17 @@
+﻿using Eplan.EplApi.Gui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST.EplAddin.CommonLibrary
+{
+    public abstract class CommonMenu
+    {
+        public void OnRegisterST() 
+        {
+
+        }
+    }
+}
