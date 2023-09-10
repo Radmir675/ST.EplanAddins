3efef67b00bbed74275d02060dcbd159cca36348
Радмир Биктимиров <tembr.com@gmail.com>
2023-09-10 13:58:12 +0300

    get process Id

---
+++ CommonLibrary/ProcessId.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Eplan.EplApi.RemoteClient;
+
+namespace ST.EplAddin.CommonLibrary
+{
+    class ProcessId
+    {
+        public int  GetEplanId() 
+        {
+            EplanServerData eplanServerData = new EplanServerData();
+            var processId=eplanServerData.EplanProcessID;
+            return processId;
+        }
+
+        public int GetProcessId() 
+        {
+            var processes = Process.GetProcessesByName("EPLAN.exe");
+            var currentProcessId = processes.Where(x => x.Id == GetEplanId()).ToString();
+            int result = int.Parse(currentProcessId);
+            return result;
+
+        }
+    }
+}
