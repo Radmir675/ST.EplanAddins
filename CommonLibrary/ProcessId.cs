cac4d9505dbe01a6c5e4fb254b5cdffff468cb50
Радмир Биктимиров <tembr.com@gmail.com>
2023-09-11 19:51:58 +0300

    1

--- CommonLibrary/ProcessId.cs
+++ CommonLibrary/ProcessId.cs
@@ -10,6 +10,9 @@ namespace ST.EplAddin.CommonLibrary
 {
     class ProcessId
     {
+        //при закртыии Eplan если Eplan.exe=1 то удалить все .json
+        //string finish = Path.Combine(Form1.Main.textBox2.Text, array[i, 0] + " " + array[i, 1] + ".txt");
+        //File.WriteAllText(finish, result);
         public int  GetEplanId() 
         {
             EplanServerData eplanServerData = new EplanServerData();
