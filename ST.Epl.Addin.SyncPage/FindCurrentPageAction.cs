586b2d1852093ab78cb53aa46755a5937b67e0ba
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-01-25 19:02:28 +0300

    начало

---
+++ ST.Epl.Addin.SyncPage/FindCurrentPageAction.cs
@@ -0,0 +1,42 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.DataModel;
+using Eplan.EplApi.HEServices;
+using System;
+using System.Collections.Specialized;
+
+namespace ST.Epl.Addin.SyncPage
+{
+    internal class FindCurrentPageAction : IEplAction
+    {
+        public static string actionName = "LastTerminalStrip";
+        public bool Execute(ActionCallingContext oActionCallingContext)
+        {
+            SelectionSet selectionSet = new SelectionSet();
+            Project currentProject = selectionSet.GetCurrentProject(true);
+            var projectName = currentProject.ProjectName;
+            selectionSet.LockProjectByDefault = false;
+            selectionSet.LockSelectionByDefault = false;
+            var currentPage = selectionSet.CurrentlyEdited;
+
+            var identifier = currentPage.ToStringIdentifier();
+            StringCollection strings = new StringCollection();
+            strings.Add(identifier);
+            Edit edit = new Edit();
+            edit.SelectObjects(projectName, strings, true);
+            edit.SetFocusToGED();
+            return true;
+        }
+
+        public void GetActionProperties(ref ActionProperties actionProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OnRegister(ref string Name, ref int Ordinal)
+        {
+            Name = actionName;
+            Ordinal = 32;
+            return true;
+        }
+    }
+}
