c5a18391e67c4d5667bb16d8686106fd80320931
Радмир <104122149+Radmir675@users.noreply.github.com>
2024-01-30 21:17:55 +0300

    Задача релизована

---
+++ CommonLibrary/CustomUserMenu.cs
@@ -0,0 +1,24 @@
+﻿using Eplan.EplApi.Gui;
+
+namespace ST.EplAddin.CommonLibrary
+{
+    public class CustomUserMenu
+    {
+        private static uint PreviousMenuId { get; set; }
+        private static string LastActionName { get; set; }
+        public bool AddMenuItem(string showingActionName, string actionName)
+        {
+            Menu menu = new Menu();
+            if (menu.GetCustomMenuId(LastActionName ?? "", null) == 0)
+            {
+                PreviousMenuId = menu.AddMainMenu("ST", Menu.MainMenuName.eMainMenuUtilities, showingActionName, actionName, "status", 1);
+            }
+            else
+            {
+                var id = menu.AddMenuItem(showingActionName, actionName, "status", PreviousMenuId, 0, true, true);
+            }
+            LastActionName = showingActionName;
+            return true;
+        }
+    }
+}
