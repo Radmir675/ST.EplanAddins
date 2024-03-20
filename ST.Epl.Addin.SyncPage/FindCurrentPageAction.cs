43815b301e1603b4fc12a1b2966855d2f35087ea
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-03-20 11:54:28 +0300

    Устранил блокировку проекта

--- ST.Epl.Addin.SyncPage/FindCurrentPageAction.cs
+++ ST.Epl.Addin.SyncPage/FindCurrentPageAction.cs
@@ -1,7 +1,8 @@
 ﻿using Eplan.EplApi.ApplicationFramework;
 using Eplan.EplApi.DataModel;
-using ST.EplAddin.SyncPage;
+using Eplan.EplApi.HEServices;
 using System;
+using System.Collections.Specialized;
 using Action = Eplan.EplApi.ApplicationFramework.Action;
 
 namespace ST.Epl.Addin.SyncPage
@@ -17,14 +18,30 @@ namespace ST.Epl.Addin.SyncPage
                 Action baseAction = oMng.FindBaseAction(this, true);
                 bool resultSync = baseAction.Execute(oActionCallingContext);
 
-                SyncPageAction syncPageAction = new SyncPageAction();
-                var result = syncPageAction.Execute();
+                var result = Start();
                 safetyPoint.Commit();
                 return result;
             }
 
         }
+        public bool Start()
+        {
+            SelectionSet selectionSet = new SelectionSet();
+            selectionSet.LockProjectByDefault = false;
+            selectionSet.LockSelectionByDefault = false;
+            var currentPage = selectionSet.CurrentlyEdited;
+            var fulLinkProject = currentPage.Project.ProjectLinkFilePath;
 
+            var identifier = currentPage.ToStringIdentifier();
+            StringCollection strings = new StringCollection();
+            strings.Add(identifier);
+            Edit edit = new Edit();
+            edit.SelectObjects(fulLinkProject, strings, true);
+            StorableObject[] storableObject = new StorableObject[1] { currentPage };
+            edit.SynchronizeObjectsToNavigators(storableObject);
+            var isFocused = edit.SetFocusToGED();
+            return true;
+        }
         public void GetActionProperties(ref ActionProperties actionProperties)
         {
             throw new NotImplementedException();
