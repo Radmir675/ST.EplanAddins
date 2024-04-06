d2fd865dbf7d89c900c4d01857f7abd3d7979ecb
Радмир <104122149+Radmir675@users.noreply.github.com>
2024-04-06 12:00:49 +0300

    22

--- ST.EplAddin.PlcEdit/ManagePlcForm.Designer.cs
+++ ST.EplAddin.PlcEdit/ManagePlcForm.Designer.cs
@@ -29,52 +29,12 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ManagePlcForm));
-            this.exchange_button = new System.Windows.Forms.Button();
-            this.up_button = new System.Windows.Forms.Button();
-            this.dowm_button = new System.Windows.Forms.Button();
             this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.exchange_button = new System.Windows.Forms.Button();
+            this.dowm_button = new System.Windows.Forms.Button();
+            this.up_button = new System.Windows.Forms.Button();
             this.flowLayoutPanel.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // exchange_button
-            // 
-            this.exchange_button.BackColor = System.Drawing.Color.Transparent;
-            this.exchange_button.BackgroundImage = global::ST.EplAddin.PlcEdit.Properties.Resources.reverse;
-            this.exchange_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.exchange_button.Location = new System.Drawing.Point(606, 3);
-            this.exchange_button.Name = "exchange_button";
-            this.exchange_button.Size = new System.Drawing.Size(25, 25);
-            this.exchange_button.TabIndex = 2;
-            this.exchange_button.TabStop = false;
-            this.exchange_button.UseVisualStyleBackColor = false;
-            // 
-            // up_button
-            // 
-            this.up_button.BackColor = System.Drawing.Color.Transparent;
-            this.up_button.BackgroundImage = global::ST.EplAddin.PlcEdit.Properties.Resources.up;
-            this.up_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.up_button.ForeColor = System.Drawing.Color.Black;
-            this.up_button.Location = new System.Drawing.Point(544, 3);
-            this.up_button.Name = "up_button";
-            this.up_button.Padding = new System.Windows.Forms.Padding(3, 0, 3, 0);
-            this.up_button.Size = new System.Drawing.Size(25, 25);
-            this.up_button.TabIndex = 1;
-            this.up_button.TabStop = false;
-            this.up_button.UseVisualStyleBackColor = false;
-            this.up_button.Click += new System.EventHandler(this.up_button_Click);
-            // 
-            // dowm_button
-            // 
-            this.dowm_button.BackColor = System.Drawing.Color.Transparent;
-            this.dowm_button.BackgroundImage = global::ST.EplAddin.PlcEdit.Properties.Resources.down;
-            this.dowm_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.dowm_button.Location = new System.Drawing.Point(575, 3);
-            this.dowm_button.Name = "dowm_button";
-            this.dowm_button.Size = new System.Drawing.Size(25, 25);
-            this.dowm_button.TabIndex = 0;
-            this.dowm_button.TabStop = false;
-            this.dowm_button.UseVisualStyleBackColor = false;
-            this.dowm_button.Click += new System.EventHandler(this.dowm_button_Click);
             // 
             // flowLayoutPanel
             // 
@@ -90,6 +50,46 @@
             this.flowLayoutPanel.Size = new System.Drawing.Size(642, 31);
             this.flowLayoutPanel.TabIndex = 3;
             this.flowLayoutPanel.WrapContents = false;
+            // 
+            // exchange_button
+            // 
+            this.exchange_button.BackColor = System.Drawing.Color.Transparent;
+            this.exchange_button.BackgroundImage = global::ST.EplAddin.PlcEdit.Properties.Resources.reverse;
+            this.exchange_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.exchange_button.Location = new System.Drawing.Point(606, 3);
+            this.exchange_button.Name = "exchange_button";
+            this.exchange_button.Size = new System.Drawing.Size(25, 25);
+            this.exchange_button.TabIndex = 2;
+            this.exchange_button.TabStop = false;
+            this.exchange_button.UseVisualStyleBackColor = false;
+            // 
+            // dowm_button
+            // 
+            this.dowm_button.BackColor = System.Drawing.Color.Transparent;
+            this.dowm_button.BackgroundImage = global::ST.EplAddin.PlcEdit.Properties.Resources.down;
+            this.dowm_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.dowm_button.Location = new System.Drawing.Point(575, 3);
+            this.dowm_button.Name = "dowm_button";
+            this.dowm_button.Size = new System.Drawing.Size(25, 25);
+            this.dowm_button.TabIndex = 0;
+            this.dowm_button.TabStop = false;
+            this.dowm_button.UseVisualStyleBackColor = false;
+            this.dowm_button.Click += new System.EventHandler(this.dowm_button_Click);
+            // 
+            // up_button
+            // 
+            this.up_button.BackColor = System.Drawing.Color.Transparent;
+            this.up_button.BackgroundImage = global::ST.EplAddin.PlcEdit.Properties.Resources.up;
+            this.up_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.up_button.ForeColor = System.Drawing.Color.Black;
+            this.up_button.Location = new System.Drawing.Point(544, 3);
+            this.up_button.Name = "up_button";
+            this.up_button.Padding = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            this.up_button.Size = new System.Drawing.Size(25, 25);
+            this.up_button.TabIndex = 1;
+            this.up_button.TabStop = false;
+            this.up_button.UseVisualStyleBackColor = false;
+            this.up_button.Click += new System.EventHandler(this.up_button_Click);
             // 
             // ManagePlcForm
             // 
