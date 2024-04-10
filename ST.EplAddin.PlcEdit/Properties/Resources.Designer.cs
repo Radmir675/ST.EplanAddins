f07df6e85ddc84d3ef875c31fffba996dce0b5f9
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-04-10 23:01:34 +0300

    добавил картинки кнопок

--- ST.EplAddin.PlcEdit/Properties/Resources.Designer.cs
+++ ST.EplAddin.PlcEdit/Properties/Resources.Designer.cs
@@ -73,6 +73,46 @@ namespace ST.EplAddin.PlcEdit.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap eplan_down {
+            get {
+                object obj = ResourceManager.GetObject("eplan_down", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap eplan_down1 {
+            get {
+                object obj = ResourceManager.GetObject("eplan_down1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap eplan_reverse {
+            get {
+                object obj = ResourceManager.GetObject("eplan_reverse", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap eplan_up {
+            get {
+                object obj = ResourceManager.GetObject("eplan_up", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap reverse {
             get {
                 object obj = ResourceManager.GetObject("reverse", resourceCulture);
