9a7c433d4388cb9bb20cdc5b8cb5f0a1be8c88c4
Радмир Биктимиров <tembr.com@gmail.com>
2024-04-20 06:52:57 +0300

    добавил класс конвертер

---
+++ ST.EplAddin.PlcEdit/CsvFileDataView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST.EplAddin.PlcEdit
+{
+    public class CsvFileDataView
+    {
+        public string DevicePointDescription { get; set; }//DI3
+        public string PLCAdress { get; set; }//I131.7
+        public string SymbolicAdress { get; set; }//DO_xLinAct2CP
+        public string FunctionText { get; set; }//Линейный актуатор 2 в позицию калибровки
+    }
+}
