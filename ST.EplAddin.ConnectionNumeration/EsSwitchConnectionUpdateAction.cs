f3e2e6e7fc217d2fa9e3f79bafd2aeea26c454f6
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-08-15 14:00:03 +0300

    1

--- ST.EplAddin.ConnectionNumeration/EsSwitchConnectionUpdateAction.cs
+++ ST.EplAddin.ConnectionNumeration/EsSwitchConnectionUpdateAction.cs
@@ -12,44 +12,44 @@ using System;
 
 namespace ST.EplAddin.ConnectionNumeration
 {
-  public class EsSwitchConnectionUpdateAction : IEplAction
-  {
-    public bool OnRegister(ref string Name, ref int Ordinal)
+    public class EsSwitchConnectionUpdateAction : IEplAction
     {
-      Name = "XWnActionRunDesignate";
-      Ordinal = 32;
-      return true;
-    }
-
-    public bool Execute(ActionCallingContext oActionCallingContext)
-    {
-      string str1 = "";
-      string str2 = "";
-      ((Context) oActionCallingContext).GetParameter("function", ref str1);
-      ((Context) oActionCallingContext).GetParameter("_cmdline", ref str2);
-      ((Context) oActionCallingContext).GetParameterCount();
-      ((Context) oActionCallingContext).GetParameters();
-      ((Context) oActionCallingContext).GetStrings();
-      SelectionSet selectionSet = new SelectionSet();
-      Project currentProject = selectionSet.GetCurrentProject(true);
-      string projectName = currentProject.ProjectName;
-      selectionSet.LockProjectByDefault = false;
-      selectionSet.LockSelectionByDefault = false;
-      Connection[] connectionsWithCf = new DMObjectsFinder(currentProject).GetConnectionsWithCF((ICustomFilter) new ConnectionFilter());
-      using (SafetyPoint safetyPoint = SafetyPoint.Create())
-      {
-        foreach (Connection connection in connectionsWithCf)
+        public bool OnRegister(ref string Name, ref int Ordinal)
         {
-          string str3 = "";
-          if (!connection.Properties.POTENTIAL_NAME.IsEmpty)
-            str3 = connection.Properties.POTENTIAL_NAME.ToString();
-          connection.Properties.CONNECTION_GROUPING.Set(str3);
+            Name = "XWnActionRunDesignate";
+            Ordinal = 32;
+            return true;
         }
-        safetyPoint.Commit();
-      }
-      return new ActionManager().FindBaseAction((IEplAction) this, true).Execute(oActionCallingContext);
-    }
 
-    public void GetActionProperties(ref ActionProperties actionProperties) => throw new NotImplementedException();
-  }
+        public bool Execute(ActionCallingContext oActionCallingContext)
+        {
+            string str1 = "";
+            string str2 = "";
+            ((Context)oActionCallingContext).GetParameter("function", ref str1);
+            ((Context)oActionCallingContext).GetParameter("_cmdline", ref str2);
+            ((Context)oActionCallingContext).GetParameterCount();
+            ((Context)oActionCallingContext).GetParameters();
+            ((Context)oActionCallingContext).GetStrings();
+            SelectionSet selectionSet = new SelectionSet();
+            selectionSet.LockProjectByDefault = false;
+            selectionSet.LockSelectionByDefault = false;
+            Project currentProject = selectionSet.GetCurrentProject(true);
+            string projectName = currentProject.ProjectName;
+            Connection[] connectionsWithCf = new DMObjectsFinder(currentProject).GetConnectionsWithCF((ICustomFilter)new ConnectionFilter());
+            using (SafetyPoint safetyPoint = SafetyPoint.Create())
+            {
+                foreach (Connection connection in connectionsWithCf)
+                {
+                    string str3 = "";
+                    if (!connection.Properties.POTENTIAL_NAME.IsEmpty)
+                        str3 = connection.Properties.POTENTIAL_NAME.ToString();
+                    connection.Properties.CONNECTION_GROUPING.Set(str3);
+                }
+                safetyPoint.Commit();
+            }
+            return new ActionManager().FindBaseAction((IEplAction)this, true).Execute(oActionCallingContext);
+        }
+
+        public void GetActionProperties(ref ActionProperties actionProperties) => throw new NotImplementedException();
+    }
 }
