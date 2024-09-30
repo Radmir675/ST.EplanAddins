5ec083798e6bc576aa7b7fb0dc385e5789620d8f
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-09-30 16:56:34 +0300

    сделал ViewModel

---
+++ ST.EplAddin.CheckCableAccesorities/Models/Class1.cs
@@ -0,0 +1,9 @@
+﻿using WpfTestApplication.ViewModels;
+
+namespace ST.EplAddin.CheckCableAccesorities.Models
+{
+    internal class Class1 : ViewModelBase
+    {
+
+    }
+}
--- ST.EplAddin.CheckCableAccesorities/ViewModels/FormViewModel.cs
+++ ST.EplAddin.CheckCableAccesorities/ViewModels/FormViewModel.cs
@@ -1,7 +1,17 @@
-﻿namespace ST.EplAddin.CheckCableAccesorities
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ST.EplAddin.CheckCableAccesorities
 {
     public class FormViewModel
     {
+        private ObservableCollection<string> _warnings { get; set; }
+        public ObservableCollection<Dictionary<string, string>> ProductRelations { get; set; }
 
+        public FormViewModel(IEnumerable<string> warnings, IEnumerable<Dictionary<string, string>> productRelations)
+        {
+            _warnings = new ObservableCollection<string>(warnings);
+            ProductRelations = new ObservableCollection<Dictionary<string, string>>(productRelations);
+        }
     }
 }
