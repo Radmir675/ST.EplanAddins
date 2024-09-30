20c9336d3ee80308ccac20cf31e1b1d1f40e374e
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-09-30 10:28:59 +0300

    WPF добавлен в проект

---
+++ ST.EplAddin.CheckCableAccesorities/WPF_Form.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ST.EplAddin.CheckCableAccesorities
+{
+    public partial class WPF_Form : UserControl
+    {
+        public WPF_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
