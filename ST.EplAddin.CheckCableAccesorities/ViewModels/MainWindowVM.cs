5778e370a886eb5271ba37553bd2b395cbcae28f
Радмир <104122149+Radmir675@users.noreply.github.com>
2024-10-04 19:58:11 +0300

    поправил фокус кнопки

--- ST.EplAddin.CheckCableAccesorities/ViewModels/MainWindowVM.cs
+++ ST.EplAddin.CheckCableAccesorities/ViewModels/MainWindowVM.cs
@@ -73,7 +73,7 @@ namespace ST.EplAddin.CheckCableAccesorities.ViewModels
                             }
                         }
                     },
-                    (obj) => PartsData.Any()));
+                    (obj) => SelectedPart != null));
             }
         }
 
