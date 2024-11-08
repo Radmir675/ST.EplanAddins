56522680b10c1377701b506b1fb1cd7c3a06ee00
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-11-08 10:56:13 +0300

    добавил пунк в меню ChangeVariantSymbol

--- ST.EplAddins.SymbolVariants/SymbolAddinModule.cs
+++ ST.EplAddins.SymbolVariants/SymbolAddinModule.cs
@@ -1,4 +1,5 @@
 ﻿using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.Gui;
 
 namespace ST.EplAddins.SymbolVariants
 {
@@ -16,7 +17,13 @@ namespace ST.EplAddins.SymbolVariants
 
         public bool OnInitGui()
         {
+            Menu menu = new Menu();
+            var menuId = menu.GetCustomMenuId("ST", null);
+            if (menuId == 0)
+                menuId = menu.AddMainMenu("ST", Menu.MainMenuName.eMainMenuUtilities, "None", "None", "Статус", 1);
+            uint subMenuID = menu.AddMenuItem("ChangeSymbolVariant", ChangeSymbolAction.actionName, "", menuId, 0, false, false);
             return true;
+
         }
 
         public bool OnRegister(ref bool bLoadOnStart)
