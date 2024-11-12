fcceb1d4a59021d124002453866b69c62729a6ae
Радмир <104122149+Radmir675@users.noreply.github.com>
2024-11-12 21:05:16 +0300

    Пока сохраним

--- ST.EplAddin.ComparisonOfProjectProperties/ViewModels/MainWindowVM.cs
+++ ST.EplAddin.ComparisonOfProjectProperties/ViewModels/MainWindowVM.cs
@@ -2,28 +2,19 @@
 using ST.EplAddin.ComparisonOfProjectProperties.Helper;
 using ST.EplAddin.ComparisonOfProjectProperties.Models;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Windows.Data;
 
 namespace ST.EplAddin.ComparisonOfProjectProperties.ViewModels
 {
     internal class MainWindowVM : ViewModelBase
     {
-        public Dictionary<int, PropertyData> FirstPropertiesList
-        {
-            get => _firstPropertiesList;
-            set
-            {
-                if (Equals(value, _firstPropertiesList)) return;
-                _firstPropertiesList = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public Dictionary<int, PropertyData> SecondPropertiesList { get; set; }
+        private CollectionViewSource _firstPropertiesCollection;
+        private CollectionViewSource _secondPropertiesCollection;
 
         public IReadOnlyList<ComparisonState> ComparisonStates { get; set; }
         private ComparisonState _selectedState = ComparisonState.None;
-        private Dictionary<int, PropertyData> _firstPropertiesList;
 
         public ComparisonState SelectedState
         {
@@ -34,26 +25,62 @@ namespace ST.EplAddin.ComparisonOfProjectProperties.ViewModels
             set
             {
                 _selectedState = value;
-                ChangeTableList();
+                OnPropertyChanged();
+                _secondPropertiesCollection.View.Refresh();
             }
         }
 
-        private void ChangeTableList()
-        {
 
-        }
+
+        public ICollectionView FirstPropertiesCollectionView => _firstPropertiesCollection?.View ?? CollectionViewSource.GetDefaultView(_firstPropertiesCollection.Source);
+
+        public ICollectionView SecondPropertiesCollectionView => _secondPropertiesCollection.View;
 
         public MainWindowVM(Dictionary<int, PropertyData> firstPropertiesList, Dictionary<int, PropertyData> secondPropertiesList) : this()
         {
-            FirstPropertiesList = firstPropertiesList;
-            SecondPropertiesList = secondPropertiesList;
+
+            _firstPropertiesCollection = new CollectionViewSource
+            {
+                Source = firstPropertiesList
+            };
+            _secondPropertiesCollection = new CollectionViewSource
+            {
+                Source = secondPropertiesList
+            };
+            _firstPropertiesCollection.Filter += _firstPropertiesCollection_Filter;
+            _secondPropertiesCollection.Filter += _secondPropertiesCollection_Filter;
         }
+        private void _firstPropertiesCollection_Filter(object sender, FilterEventArgs e)
+        {
+
+            e.Accepted = true;
+
+        }
+
+        private void _secondPropertiesCollection_Filter(object sender, FilterEventArgs e)
+        {
+            switch (SelectedState)
+            {
+                case ComparisonState.Difference:
+                    if (e.Item is Dictionary<int, PropertyData> data)
+                    {
+                        e.Accepted = true;
+
+                    }
+                    break;
+                case ComparisonState.None:
+                    e.Accepted = true;
+
+                    break;
+                case ComparisonState.Similarity:
+                    break;
+            }
+        }
+
 
         public MainWindowVM()
         {
             ComparisonStates = EnumExtension.GetValues<ComparisonState>().ToList();
         }
-        public string SelectedObject { get; set; }
-
     }
 }
