4808fd3850cab0c938818d6ec92e68b29112b516
Радмир <104122149+Radmir675@users.noreply.github.com>
2024-11-15 18:20:22 +0300

    добавил конвертер

---
+++ ST.EplAddin.ComparisonOfProjectProperties/Converters/ColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ST.EplAddin.ComparisonOfProjectProperties.Converters
+{
+    internal class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ChangesRecord changesRecord = new ChangesRecord();
+            var recordList = changesRecord.GetChangesList();
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
