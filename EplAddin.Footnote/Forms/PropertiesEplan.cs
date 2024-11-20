b4a7fbe2ce3c558208ddbc4c20076e238625a6ec
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-11-20 16:28:50 +0300

    удаление footnote

--- EplAddin.Footnote/Forms/PropertiesEplan.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ST.EplAddin.Footnote.ProperyBrowser
-{
-    public class PropertiesEplan
-    {
-        public string Property { get; set; }
-        public string Value { get; set; }
-    }
-}
