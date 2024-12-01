37c6db6d40f5f4b201c3895b60f0b8bed5e285a0
Радмир <104122149+Radmir675@users.noreply.github.com>
2024-12-01 09:22:48 +0300

    добавил отображение charactermap

---
+++ ST.EplAddin.FootNote/ViewModels/PropertiesWindowVM.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ST.EplAddin.FootNote.ViewModels
+{
+    internal class PropertiesWindowVM
+    {
+        private void ShowCharacterMap()
+        {
+            try
+            {
+                Process.Start("charmap.exe");
+            }
+            catch (Win32Exception e)
+            {
+                MessageBox.Show("Таблица символов не установлена!",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+    }
+}
