91ef77fd23bd7390dfc5207fd2cd018ab75c1c78
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-12-02 18:45:16 +0300

    добавил viewmodel

---
+++ ST.EplAddin.FootNote/Models/Class1.cs
@@ -0,0 +1,9 @@
+﻿using ST.EplAddin.FootNote.ViewModels;
+
+namespace ST.EplAddin.FootNote.Models
+{
+    internal class Class1 : ViewModelBase
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ST.EplAddin.FootNote/ViewModels/ViewModelBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ST.EplAddin.FootNote.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly Dictionary<string, object> _values = new();
+        protected void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged();
+            return true;
+        }
+
+        protected bool Set<T>(T value, [CallerMemberName] string propertyName = null!)
+        {
+            if (_values.TryGetValue(propertyName, out var old_value) && Equals(old_value, value)) return false;
+            _values[propertyName] = value;
+            OnPropertyChanged();
+            return true;
+        }
+        protected T Get<T>([CallerMemberName] string propertyName = null!)
+        {
+            return _values.TryGetValue(propertyName, out var value) ? (T)value : default;
+        }
+
+    }
+}
