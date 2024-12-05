f93d402bf1bf8cd1a2c4910a3b27cf9583b31560
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-12-05 16:37:02 +0300

    Добавил новый action

---
+++ ST.EplAddin.UserConfigurationService/ConfigurationAction.cs
@@ -0,0 +1,35 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.HEServices;
+
+namespace ST.EplAddin.UserConfigurationService
+{
+    public class ConfigurationAction : IEplAction
+    {
+
+        public static string actionName = "UserConfigurationService";
+
+        public bool OnRegister(ref string Name, ref int Ordinal)
+        {
+            Name = actionName;
+            Ordinal = 99;
+            return true;
+        }
+
+        public bool Execute(ActionCallingContext oActionCallingContext)
+        {
+            SelectionSet selectionSet = new SelectionSet
+            {
+                LockProjectByDefault = false,
+                LockSelectionByDefault = false
+            };
+
+            return true;
+
+        }
+
+        public void GetActionProperties(ref ActionProperties actionProperties)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
