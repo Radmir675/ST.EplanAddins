114cefd2020a4eb1fc18e884a0db6d29ae4e0435
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-12-09 13:49:33 +0300

    Биндим данные

--- ST.EplAddin.UserConfigurationService/ViewModels/ConfigurationVM.cs
+++ ST.EplAddin.UserConfigurationService/ViewModels/ConfigurationVM.cs
@@ -1,7 +1,21 @@
-﻿namespace ST.EplAddin.UserConfigurationService.ViewModels
+﻿using ST.EplAddin.UserConfigurationService.Models;
+
+namespace ST.EplAddin.UserConfigurationService.ViewModels
 {
     internal class ConfigurationVM : ViewModel
     {
+        public UserConfigurationShemes _userConfigurationShemes { get; set; }
+
+
+        public ConfigurationVM(UserConfigurationShemes userConfigurationShemes)
+        {
+            _userConfigurationShemes = userConfigurationShemes;
+        }
+
+        public ConfigurationVM()
+        {
+
+        }
 
     }
 }
