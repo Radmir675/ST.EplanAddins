941f54735055890e4fc477b6c917c3afb0d700c0
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2024-12-09 12:57:28 +0300

    Описание запрограммировали

---
+++ ST.EplAddin.UserConfigurationService/Models/Schema.cs
@@ -0,0 +1,9 @@
+﻿namespace ST.EplAddin.UserConfigurationService.Models
+{
+    internal class Schema(string name, string description)
+    {
+
+        public string Name { get; set; } = name;
+        public string Description { get; set; } = description;
+    }
+}
--- ST.EplAddin.UserConfigurationService/ViewModels/SchemesVM.cs
+++ ST.EplAddin.UserConfigurationService/ViewModels/SchemesVM.cs
@@ -1,6 +1,13 @@
-﻿namespace ST.EplAddin.UserConfigurationService.ViewModels
+﻿using ST.EplAddin.UserConfigurationService.Models;
+using System.Collections.ObjectModel;
+
+namespace ST.EplAddin.UserConfigurationService.ViewModels
 {
     internal class SchemesVM : ViewModel
     {
+        public ObservableCollection<Schema> Collection { get; set; } = new();
+        public Schema SelectedItem { get; set; }
+        public RelayCommand OkCommand { get; set; }
+
     }
 }
