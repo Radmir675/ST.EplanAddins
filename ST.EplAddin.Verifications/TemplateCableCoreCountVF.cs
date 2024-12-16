380de05c10830459180a18143c6b63aa29e56e12
Радмир <104122149+Radmir675@users.noreply.github.com>
2024-12-16 21:33:02 +0300

    добавил новую проверку

---
+++ ST.EplAddin.Verifications/TemplateCableCoreCountVF.cs
@@ -0,0 +1,42 @@
+﻿using Eplan.EplApi.DataModel;
+using Eplan.EplApi.EServices;
+
+namespace ST.EplAddin.Verifications
+{
+    internal class TemplateCableCoreCountVF : Verification
+    {
+        private const int m_iMessageId = 40;
+        public override void OnRegister(ref string strName, ref int iOrdinal)
+        {
+            strName = "TemplateCableCoreCountVF";
+            iOrdinal = 30;
+            this.VerificationPermission = IVerification.Permission.OnlineOfflinePermitted;
+            this.VerificationState = IVerification.VerificationState.OnlineOfflineState;
+        }
+
+        public override void OnStartInspection(bool bOnline) { }
+
+        public override void OnEndInspection() { }
+
+        public override void Execute(StorableObject oObject1)
+        {
+        }
+
+        public override void OnRegister(ref string strCreator, ref IMessage.Region eRegion, ref int iMessageId, ref IMessage.Classification eClassification,
+            ref int iOrdinal)
+        {
+            strCreator = "Scantronic";
+            eRegion = IMessage.Region.Externals;
+            iMessageId = m_iMessageId;
+            eClassification = IMessage.Classification.Error;
+            iOrdinal = 20;
+        }
+
+        public override string GetMessageText()
+        {
+            return "В кабеле %1!s! не присвоены жилы";
+        }
+
+        public override void DoHelp() { }
+    }
+}
