e4b45740ebff99518f2bbcf7d9465a173c84b573
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2025-03-04 11:37:10 +0300

    Навигатор добавлен

---
+++ ST.EplAddin.PlacementNavigator/AddinModule.cs
@@ -0,0 +1,46 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.Gui;
+using Eplan.EplSDK.WPF;
+
+namespace ST.EplAddin.PlacementNavigator
+{
+    public class PlacementNavigator : IEplAddIn
+    {
+        EventHandler m_EventHandler;
+        public bool OnExit()
+        {
+            return true;
+        }
+
+        public bool OnInit()
+        {
+            return true;
+        }
+
+        public bool OnInitGui()
+        {
+            Menu menu = new Menu();
+            var menuId = menu.GetCustomMenuId("ST", null);
+            if (menuId == 0)
+                menuId = menu.AddMainMenu("ST", Menu.MainMenuName.eMainMenuUtilities, "None", "None", "Статус", 1);
+            uint subMenuID = menu.AddMenuItem(
+                "Навигатор символов", "GfDialogManagerShow /DialogName:SymbolsNavigator", "", menuId, 0, false, false);
+
+            //Регистрация навигатора
+            var className = "SymbolsNavigator"; //MethodBase.GetCurrentMethod().DeclaringType.Name;
+            DialogBarFactory dialogBarFactory = new DialogBarFactory(className, typeof(Navigator), DialogDockingOptions.Any, 0);
+            return true;
+        }
+
+        public bool OnRegister(ref bool bLoadOnStart)
+        {
+            bLoadOnStart = true;
+            return true;
+        }
+
+        public bool OnUnregister()
+        {
+            return true;
+        }
+    }
+}
---
+++ ST.EplAddin.PlacementNavigator/Navigator.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Eplan.EplSDK.WPF.Interfaces;
+using Eplan.EplSDK.WPF.Interfaces.DialogServices;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ST.EplAddin.PlacementNavigator
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class Navigator : UserControl, IDialog, IDialogBar, IDialogComponentAccess, ICallingContext, IDialogState, IDialogAction, IDialogClose, IElementStateAccess
+    {
+        public string Caption { get; }
+        public bool IsTabsheet { get; }
+        public int UniqueBarID { get; }
+        public IDialogComponent Component { get; set; }
+        public object Context { get; set; }
+        public IDialogStateManager DialogStateManager { get; set; }
+        public IElementStateCollection ElementStateCollection { get; }
+
+        public Navigator()
+        {
+            // Init
+            ElementStateCollection = new Eplan.EplSDK.WPF.Controls.Persistency.ElementStateCollection();
+            ElementStateCollection.Load(nameof(Navigator));
+
+            InitializeComponent();
+            Caption = "Навигатор символов";
+            IsTabsheet = false;
+
+            // Use Class name for uniqueid
+            // ReSharper disable once PossibleNullReferenceException
+            var className = MethodBase.GetCurrentMethod().DeclaringType.Name;
+            UniqueBarID = className.GetHashCode();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void init()
+        {
+        }
+
+        public bool isValid()
+        {
+            return true;
+        }
+
+        public void reload()
+        {
+            this.InvalidateVisual();
+        }
+
+        public void save()
+        {
+        }
+
+        public void close()
+        {
+        }
+
+    }
+}
