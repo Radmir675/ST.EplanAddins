35b0cc03e4f8c4fcc96ade7d2147f974ee028dc5
Радмир <104122149+Radmir675@users.noreply.github.com>
2025-03-23 00:13:02 +0300

    Добавил правильный футнот

---
+++ ST.EplAddin.FootNote/FootNote/Footnote_PropertiesAction.cs
@@ -0,0 +1,63 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.DataModel;
+using Eplan.EplApi.HEServices;
+using ST.EplAddin.FootNote.ProperyBrowser;
+using System;
+using Action = Eplan.EplApi.ApplicationFramework.Action;
+
+namespace ST.EplAddin.FootNote.FootNote
+{
+    class Footnote_PropertiesAction : IEplAction
+    {
+        public bool Execute(ActionCallingContext oActionCallingContext)
+        {
+            string function = "", cmdline = "";
+            oActionCallingContext.GetParameter("function", ref function);
+            oActionCallingContext.GetParameter("_cmdline", ref cmdline);
+
+            int count = oActionCallingContext.GetParameterCount();
+            string[] contextParams = oActionCallingContext.GetParameters();
+            string[] contextStrings = oActionCallingContext.GetStrings();
+
+            if (function == "PropertiesRedundant")
+            {
+                SelectionSet selectionSet = new SelectionSet();
+                selectionSet.LockProjectByDefault = false;
+                selectionSet.LockSelectionByDefault = false;
+                Project CurrentProject = selectionSet.GetCurrentProject(true);
+                StorableObject storableObject = selectionSet.GetSelectedObject(true);
+                if (storableObject is Block block)
+                {
+
+                    if (FootnoteVerification.IsFootnoteBlock(block))
+                    {
+                        FootNote.FootnoteItem footnoteItem = new FootNote.FootnoteItem();
+                        footnoteItem.Create(block);
+                        PropertiesDialogForm propertiesDialog = new PropertiesDialogForm();
+                        propertiesDialog.SetItem(footnoteItem);
+                        propertiesDialog.ShowDialog();
+
+                        return true;
+                    }
+                }
+            }
+
+            ActionManager oMng = new ActionManager();
+            Action baseAction = oMng.FindBaseAction(this, true);
+            return baseAction.Execute(oActionCallingContext);
+        }
+
+        public void GetActionProperties(ref ActionProperties actionProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OnRegister(ref string Name, ref int Ordinal)
+        {
+            Name = "GfDlgMgrActionIGfWind";
+            Ordinal = 99;
+            return true;
+
+        }
+    }
+}
---
+++ ST.EplAddin.FootNote/PropertyBrowser/EplHelper.cs
@@ -0,0 +1,22 @@
+﻿using Eplan.EplApi.Base;
+using System;
+
+namespace ST.EplAddin.FootNote
+{
+    public class EplHelper
+    {
+        public static ISOCode.Language GuiLanguage { get { return new Languages().GuiLanguage.GetNumber(); } }
+    }
+    public class WindowWrapper : System.Windows.Forms.IWin32Window
+    {
+        public WindowWrapper(IntPtr handle)
+        {
+            _hwnd = handle;
+        }
+        public IntPtr Handle
+        {
+            get { return _hwnd; }
+        }
+        private IntPtr _hwnd;
+    }
+}
