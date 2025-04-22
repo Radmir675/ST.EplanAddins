071a2ffc514543f1bb0ba224321c9eff91b62afb
Радмир Биктимиров <biktimirov.rr@edu.spbstu.ru>
2025-04-22 08:36:15 +0300

    практический написано

--- ST.EplAddin.Verifications/LengthPartVF.cs
+++ ST.EplAddin.Verifications/LengthPartVF.cs
@@ -1,11 +1,14 @@
 ﻿using Eplan.EplApi.DataModel;
+using Eplan.EplApi.DataModel.E3D;
 using Eplan.EplApi.EServices;
+using System;
 
 namespace ST.EplAddin.Verifications
 {
     internal class LengthPartVF : Verification
     {
         private const int m_iMessageId = 43;
+        private const int TOLERANCE = 2;
         public override void OnRegister(ref string strName, ref int iOrdinal)
         {
             strName = "LengthPartVF";
@@ -25,11 +28,24 @@ namespace ST.EplAddin.Verifications
         public override void Execute(StorableObject oObject1)
         {
             if (oObject1 == null) return;
-            if (oObject1 is ViewPart) return;
+            if (oObject1 is not BusBar busBar) return;
+            var length3D = Get3DLength(busBar);
+            var length2D = Get2DLength(busBar);
+            if (Math.Abs(length2D - length3D) > TOLERANCE)
+            {
+                DoErrorMessage(oObject1, busBar.Name);
+            }
+        }
 
+        private double Get2DLength(BusBar busBar)
+        {
+            return 0;
+        }
 
-
-
+        private double Get3DLength(BusBar busBar)
+        {
+            var length = busBar.ArticleReferences[0]?.Properties[31008].ToDouble();
+            return length ?? 0;
         }
 
         public override void OnRegister(ref string strCreator, ref IMessage.Region eRegion, ref int iMessageId, ref IMessage.Classification eClassification,
@@ -44,7 +60,7 @@ namespace ST.EplAddin.Verifications
 
         public override string GetMessageText()
         {
-            return "Длина %1!s! не соответствует главной функции";
+            return "Длина %1!s! не соответствует главной функции в 2D представлении";
         }
 
         public override void DoHelp()
